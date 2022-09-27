4888525aa918b32cb048df589b8d781e41a0c17a
Roni Kreinin <rkreinin@clearpathrobotics.com>
2022-09-27 13:52:18 -0400

    View diagnostics with rqt_robot_monitor

--- jackal_viz/launch/view_diagnostics.launch.py
+++ jackal_viz/launch/view_diagnostics.launch.py
@@ -1,22 +1,15 @@
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
-    config_arg = DeclareLaunchArgument(
-        name="config",
-        default_value="diagnostics",
-    )
-    rqt_dashboard_node = Node(
-        package='rqt_gui',
-        executable='rqt_gui',
-        name='rqt_dashboard',
-        arguments=['--perspective-file ', get_package_share_directory('jackal_viz'), '/rqt/', LaunchConfiguration('config'), '.perspective'],
-        output='screen',
-    )
-    return LaunchDescription([
-        config_arg,
-        rqt_dashboard_node
-    ])
+
+    robot_monitor = Node(
+        package='rqt_robot_monitor',
+        executable='rqt_robot_monitor',
+        output='screen')
+
+    ld = LaunchDescription()
+    ld.add_action(robot_monitor)
+    return ld
